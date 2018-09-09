17a7923198287be5785adf55ed4bb0d110cac2f3
Zaky Rahim <zaky.rahim@gmail.com>
2018-09-09 13:47:52 +0700

    init repo - react with next.js

---
+++ pages/create-bid.js
@@ -0,0 +1,20 @@
+export default () => 
+    <div> 
+        <span>create bid</span>
+        Nama Barang
+        <input type="text"/>
+        Photo
+
+        Harga *Langsung Jual
+        <input type="text"/>
+        Mulai Bidding
+        <input type="text"/>
+        Kelipatan Bidding:
+        <input type="text"/>
+
+        Tanggal Mulai Bidding:
+        <input type="text"/>
+        Tanggal Akhir Bidding:
+        <input type="text"/>
+
+    </div>
---
+++ pages/index.js
@@ -0,0 +1,3 @@
+import Link from 'next/link'
+export default ()=>
+    <div> Hello Bidding...!!! click  <Link href="/create-bid">here</Link> to create bid </div>
